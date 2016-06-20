688949c8f98555c3b976bbd7578802d5104569eb
Roy Seto <roy.seto@gmail.com>
2016-06-19 21:13:32 -0700

    Update install_phantomjs.sh to 2.1.1

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-# This script installs PhantomJS.
+# This script must be run sudo root as follows:
+#
+#     sudo ./root_install_phantomjs.sh
+#
+# It downloads and installs phantomjs on Ubuntu Linux and has been tested on
+# Ubuntu 12.04.
+#
+# This script can be safely rerun. It will overwrite a prior installation
+# if it exists.
+#
 # It is adapted from the following procedure:
 # http://kb.solarvps.com/ubuntu/installing-phantomjs-on-ubuntu-12-04-lts/
 #
@@ -8,12 +17,12 @@
 # http://phantomjs.org/download.html
 #
 
-PHANTOMJS_VER=1.9.2
+PHANTOMJS_VER=2.1.1
 PJS=phantomjs-$PHANTOMJS_VER-linux-x86_64
 
-mkdir -p /usr/local/share
+mkdir /usr/local/share
 cd /usr/local/share
-wget https://phantomjs.googlecode.com/files/$PJS.tar.bz2
+wget https://bitbucket.org/ariya/phantomjs/downloads/$PJS.tar.bz2
 tar xjf $PJS.tar.bz2
 ln -sf /usr/local/share/$PJS/bin/phantomjs /usr/local/share/phantomjs
 ln -sf /usr/local/share/$PJS/bin/phantomjs /usr/local/bin/phantomjs
