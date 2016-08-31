0e731ede067678809dd8b40b9b8c57d2a0da6223
Roy Seto <royseto@users.noreply.github.com>
2016-08-31 12:46:01 -0700

    Add install_pypy.sh

---
+++ install_pypy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script downloads and installs PyPy and makes it available as
+# /usr/local/bin/pypy. It will work only on 64-bit Ubuntu Linux.
+# This must be run as root.
+
+cd /tmp
+wget https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.3.1-linux64.tar.bz2
+cd /usr/local/lib
+tar xf /tmp/pypy2-v5.3.1-linux64.tar.bz2
+ln -sf /usr/local/lib/pypy2-v5.3.1-linux64/bin/pypy /usr/local/bin/pypy
+rm -f /tmp/pypy2-v5.3.1-linux64.tar.bz2
