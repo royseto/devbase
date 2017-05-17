aa74ac78d784f0e4fbd21db59852b25eba43b46e
Roy Seto <royseto@users.noreply.github.com>
2017-05-17 10:10:28 -0700

    Update PyPy from 5.6.0 to 5.7.1

--- install_pypy.sh
+++ install_pypy.sh
@@ -5,8 +5,8 @@
 # This must be run as root.
 
 cd /tmp
-wget https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.6.0-linux64.tar.bz2
+wget https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.7.1-linux64.tar.bz2
 cd /usr/local/lib
-tar xf /tmp/pypy2-v5.6.0-linux64.tar.bz2
-ln -sf /usr/local/lib/pypy2-v5.6.0-linux64/bin/pypy /usr/local/bin/pypy
-rm -f /tmp/pypy2-v5.6.0-linux64.tar.bz2
+tar xf /tmp/pypy2-v5.7.1-linux64.tar.bz2
+ln -sf /usr/local/lib/pypy2-v5.7.1-linux64/bin/pypy /usr/local/bin/pypy
+rm -f /tmp/pypy2-v5.7.1-linux64.tar.bz2
